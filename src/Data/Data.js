77f37152f6596f00a71a2566a85babd2b9880143
ProgrammerSnigdho <programmersnigdho@gmail.com>
2021-01-10 21:38:21 +0600

    first-commit

---
+++ src/Data/Data.js
@@ -0,0 +1,45 @@
+const Data = [
+    {
+        title: 'React For Beginners',
+        instructor: 'Programming Simplified',
+        length: '3 Hours and 30 Minutes',
+        students: '10.9M',
+        price: 29.99
+    },
+    {
+        title: 'JavaScript Crash Course',
+        instructor: 'Programming Simplified',
+        length: '9 Hours and 45 Minutes',
+        students: '6.9M',
+        price: 45.99
+    },
+    {
+        title: 'Deep Learning For Nerds',
+        instructor: 'Programming Simplified',
+        length: '30 Hours',
+        students: '4.2M',
+        price: 150.99
+    },
+    {
+        title: 'Getting Started With Machine Learning In Python',
+        instructor: 'Programming Simplified',
+        length: '6 Hours and 15 Minutes',
+        students: '13.4M',
+        price: 35.99
+    },
+    {
+        title: 'Python For Nerds',
+        instructor: 'Programming Simplified',
+        length: '9 Hours and 45 Minutes',
+        students: '6.9M',
+        price: 45.99
+    },
+    {
+        title: 'Deep Learning For Nerds',
+        instructor: 'Programming Simplified',
+        length: '30 Hours',
+        students: '4.2M',
+        price: 150.99
+    }
+]
+export default Data;
